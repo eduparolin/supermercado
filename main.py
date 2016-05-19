24833f23ff7754e418834ecf508370ba7fa23bab
_ <git15@inf.ufpr.br>
2016-05-19 11:09:42 -0300

    Comentei

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import ponto.py
 
+#so faz  merda
 #comentario
 main:
 	#printf('esse é um supermercado');
 
 caixa:
+	#como sempre fazendo merda
 	#Ligar esteira
 	#Ler codigo de barras
 	#calculo do preco de cada produto
